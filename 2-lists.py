afb5d5fe0cbc22c0f21ebc87cc0a36b0f1acfbe6
SugiraVinc <sugiravincentg11@gmail.com>
2022-07-20 01:09:55 +0200

    Positioning an integer

---
+++ 2-lists.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def num_sort(num, target):
+    num.sort()
+    if not num:
+        return 0
+    if target > num[-1]:
+        return len(num)
+    if target < num[0]:
+        return 0
+    for i in range(len(num)):
+        if num[i] >= target:
+            return i
